92819d21d6935de0ec56484c16d890530ac2ee1b
HannaK-Git <annakovally@gmail.com>
2022-09-09 12:02:22 +0300

    link was added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 import CoverPage from "./CoverPage/cover";
+import HomePage from "./Home/HomePage";
 
 function App() {
   return (
     <>
       <Routes>
         <Route exact path="/" element={<CoverPage></CoverPage>} />
+        <Route path="/home" element={<HomePage></HomePage>} />
       </Routes>
     </>
   );
