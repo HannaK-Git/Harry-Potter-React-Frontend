d41cf27d0a66d90b6ca7dd9bb118bad5567596ac
HannaK-Git <annakovally@gmail.com>
2022-09-09 11:03:07 +0300

    cover page

---
+++ src/CoverPage/cover.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import hodCover from "../images/hogCover.jpg";
+import styles from "./cover.module.css";
+
+const CoverPage = () => {
+  return (
+    <div className={styles.mainD}>
+      <div className={styles.img}>
+        <Link className={styles.linkCov} to="/home">Let the Journey Begin</Link>
+      </div>
+    </div>
+  );
+};
+
+export default CoverPage;
