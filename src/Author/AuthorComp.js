a74a9d60aa49f248f40c677401a05984ea90cc29
HannaK-Git <annakovally@gmail.com>
2022-09-10 12:27:49 +0300

    make it better

--- src/Author/AuthorComp.js
+++ src/Author/AuthorComp.js
@@ -3,10 +3,14 @@ import NavBarComp from "../NavBar/NavBarComp";
 import FooterComp from "../Footer/FooterComp";
 import jkr from "../images/jk-rowling.jpg";
 import styles from "./author.module.css";
-
 import back from "../images/background.jpg";
 import friends from "../images/friends.png";
 
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 
 
@@ -77,11 +81,53 @@ const AuthorComp = () => {
       </section>
       <section className={styles.section3}>
         <figure>
-          <img src={friends} alt="friends"/>
+          <img src={friends} alt="friends" />
         </figure>
       </section>
+      <div>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>Accordion 1</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+              eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+          >
+            <Typography>Accordion 2</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+              eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion disabled>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel3a-content"
+            id="panel3a-header"
+          >
+            <Typography>Disabled Accordion</Typography>
+          </AccordionSummary>
+        </Accordion>
+      </div>
 
-      
       <FooterComp></FooterComp>
     </div>
   );
